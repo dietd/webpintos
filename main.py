0a4d8240af87fde4e27ef1b673f51781c32b9a39
Diether Delosreyes <dietherd@berkeley.edu>
2021-06-17 20:27:19 +0000

    try to deploy

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 from flask import Flask, render_template, request
 from flask_restful import reqparse, abort, Api, Resource
 from flask_socketio import SocketIO
-import os_listener
+import shell
 import io
 import threading
 
+import eventlet
+eventlet.monkey_patch()
+
 app = Flask(__name__)
 socketio = SocketIO(app)
-shell = os_listener.Shell(socketio, "pintos -v -k --qemu --disk cs162proj.dsk -- -q run shell")
-
-# This is the function thread calls to output the shell output
-def shell_output():
-    buf = io.StringIO()
-    while True:
-        data = shell.p.stdout.read(1)
-        if data:
-            buf.write(data.decode("utf-8"))
-        else:
-            if (buf.getvalue() != ""):
-                socketio.emit('send_output', buf.getvalue())
-                buf = io.StringIO()
-
-thread = threading.Thread()
+shell = shell.Shell(socketio, "../bin/pintos -v -k --qemu --disk cs162proj.dsk -- -q run shell")
 
 @app.route('/')
 def home():
@@ -29,20 +18,18 @@ def home():
 
 @socketio.on('connect')
 def connect():
-    shell.run()
-    global thread
-    if not thread.isAlive():
-        thread = socketio.start_background_task(shell_output)
+    #shell.run()
+    if not shell.output_thread.isAlive():
+        socketio.start_background_task(shell.output)
 
 @socketio.on('disconnect')
 def disconnect():
     shell.input("halt\r")
-    global thread
-    thread.join()
 
 @socketio.on('send_input')
 def shell_input(msg, methods=['POST']):
     shell.input(msg + '\r')
     
 if __name__ == '__main__':
-    socketio.run(app, host='192.168.162.162', port=8000, debug=True)
+    shell.run()
+    socketio.run(app, host='192.168.162.162', port=8000, debug=False)
---
+++ shell.py
@@ -0,0 +1,47 @@
+import subprocess
+import threading
+import io
+from fcntl import fcntl, F_GETFL, F_SETFL
+from os import O_NONBLOCK
+import sys
+#from flask_socketio import SocketIO
+
+command = "../bin/pintos -v -k --qemu --disk cs162proj.dsk -- -q run shell"
+
+class Shell():
+    def set_flags(self, pipe):
+        flags = fcntl(pipe, F_GETFL)
+        fcntl(pipe, F_SETFL, flags | O_NONBLOCK)
+    
+    def __init__(self, app, command):
+        self.app = app
+        self.cmd = command
+        self.p = None
+        self.output_thread = threading.Thread()
+
+    def run(self):
+        if self.p == None:
+            self.p = subprocess.Popen(self.cmd.split(' '),
+                    cwd="./os_build",
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE,
+                    )
+            self.set_flags(self.p.stdout)
+            self.set_flags(self.p.stdin)
+
+    def output(self):
+        buf = io.StringIO()
+        while True:
+            data = self.p.stdout.read(1)
+            if data:
+                buf.write(data.decode('utf-8'))
+            else:
+                if (buf.getvalue() != ""):
+                    self.app.emit('send_output', buf.getvalue())
+                    buf = io.StringIO()
+
+    def input(self, command):
+        self.p.stdin.write(command.encode())
+        self.p.stdin.flush()
+
+shell = Shell(None, command)
